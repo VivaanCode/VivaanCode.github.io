5539735fbc01a6806a46ef8ee19a51dc76fe850c
Vivaan <87912804+VivaanCode@users.noreply.github.com>
2026-02-25 19:53:55 +0000

    might not be the best way to learn svelte/sveltekit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import favicon from '$lib/assets/avatar.png';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<main>
+	{@render children()}
+</main>
