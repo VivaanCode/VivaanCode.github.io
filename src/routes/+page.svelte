1e99521b7db2f2baddaee5cc7af283f322e3f216
Vivaan <87912804+VivaanCode@users.noreply.github.com>
2026-02-25 20:37:46 +0000

    so i love torturing myself so i also installed tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,36 @@
+<script>
+    import { onMount } from 'svelte';
+    
+    let shouldFade = false;
+    
+    onMount(() => {
+        setTimeout(() => {
+            shouldFade = true;
+        }, 75);
+    });
+</script>
+
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+    /* @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,1...'); */
+    @reference "tailwindcss";
 
-
-    :global(body) {
+    :global(html, body) {
         background: #000;
         color: #fff;
-        font-family: "Roboto", sans-serif;
+        margin: 0;
+        height: 100%;
+    }
+
+    h1 {
+        font-family: "Inter", sans-serif;
+        text-align: center;
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: 0;
     }
 
 </style>
 
-<h1>hi</h1>
+<h1 class="text-3xl font-semibold {shouldFade ? 'animate-fadeOut' : ''}">vivaan</h1> <!-- autocomplete really pulling its weight with this templating -->
